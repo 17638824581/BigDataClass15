a1baaebfbf8989677073b069c2687b081ad684a1
小邢同学 <1294992675@qq.com>
2022-12-29 11:33:05 +0800

    12 月 29 日，新增 JavaScript 相关内容

---
+++ BigDataClass15/pro20-WebPage/web/js/helloJs.js
@@ -0,0 +1,10 @@
+// js 中的注释：// 单行注释
+// js 中的多行注释：  /* 多行注释 */
+/*
+*  当时学习Java的时候：  System.out.println("Hello World!");
+*
+* */
+
+// 打印 Hello World！
+// 方式1：使用 document.write(); 函数（方法）
+document.write("Hello World!")
